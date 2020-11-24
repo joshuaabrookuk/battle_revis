17d148fe28895acc937d99d97d38ed2509a4c441
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-11-24 15:29:02 +0000

    GREEN: Two players enter thier names in a form. Names are 'vs' in a new route. Posting params

--- spec/features/players_form_and_display_spec.rb
+++ spec/features/players_form_and_display_spec.rb
@@ -3,10 +3,9 @@ require 'sinatra'
 feature "Ready two players" do
   scenario "Two players enter thier names in a form" do
   visit '/'
-  fill_in('Player_1', with: 'John')
-  fill_in('Player_2', with: 'Suze')
+  fill_in('player_1', with: 'John')
+  fill_in('player_2', with: 'Suze')
   click_button 'Submit'
-  expect(page).to have_content("John")
-  expect(page).to have_content("Suze")
+  expect(page).to have_content("John vs Suze")
   end
 end
