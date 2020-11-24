18c68faf31bdbfefbff2f04f015e0f757dec8d7f
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-11-24 16:20:31 +0000

    REFRACTOR: Using sessions to store our values and creating a new get requrest on 'play'

--- my_app.rb
+++ my_app.rb
@@ -1,15 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-  erb :names
-end
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect to '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb :play
+  end
 
   run! if app_file == $0
 end
