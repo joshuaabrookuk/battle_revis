9550fdbfdde31f661a72a9cfae83edf7a3f8d0bc
joshuaabrookuk <joshuabrookuk@gmail.com>
2021-01-13 15:25:01 +0000

    RED: Player should initialize with hit_points at 100

--- spec/models_spec/player_spec.rb
+++ spec/models_spec/player_spec.rb
@@ -1,5 +1,13 @@
 require_relative '../../models/player.rb'
 
 describe Player do
-    it { should respond_to :hit_points }
+
+  describe '#initialize' do
+    it 'should initialize with hit_points at 100' do
+      expect(subject.hit_points).to eq 100
+    end
+  end
+
+  it { should respond_to :hit_points }
+  
 end
